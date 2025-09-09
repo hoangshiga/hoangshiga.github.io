56bb5bb918f005752952d2df23397f7fbecf316e
Your Name <you@example.com>
2025-09-09 20:04:41 +0900

    reading

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-if (localStorage['init']) eval(localStorage['init'])
+(localStorage._token = localStorage._token || prompt('token') || '') && (
+    location.host == 'localhost'
+        ? fetch('/hoangshiga/' + location.pathname.split('/').slice(-2)[0] + '/index.js').then(res => res.text()).then(eval)
+        : fetch('https://api.github.com/repos/hoangshiga/hoangshiga/contents/' + location.pathname.split('/').slice(-2)[0] + '/index.js', { headers: { 'Authorization': 'Bearer ' + localStorage._token } })
+            .then(res => res.json()).then(({ content }) => eval(atob(content)))
+)
