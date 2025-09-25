62aa7b07b95f3473d38616c049264d5e0399da0d
chuonglntm <chuonglntm@gmail.com>
2025-09-25 21:14:59 +0900

    ./index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-(localStorage._token = localStorage._token || prompt('token') || '') && (
+if (location.pathname == '/login/') {
+    var input = document.createElement('input')
+    input.type = 'password'
+    var button = document.createElement('button')
+    button.textContent = 'Login'
+    button.onclick = () => (localStorage._token = input.value) && localStorage._redirectUrl && location.replace(localStorage._redirectUrl)
+    document.body.append(input, button)
+} else {
+    delete localStorage._redirectUrl
     location.host == 'localhost'
         ? fetch('/hoangshiga/' + location.pathname.split('/').slice(-2)[0] + '/index.js').then(res => res.text()).then(eval)
         : fetch('https://api.github.com/repos/hoangshiga/hoangshiga/contents/' + location.pathname.split('/').slice(-2)[0] + '/index.js', { headers: { 'Authorization': 'Bearer ' + localStorage._token } })
             .then(res => res.json()).then(({ content }) => eval(atob(content)))
-)
+            .catch(ex => (localStorage._redirectUrl = location.href, location.replace('/login/')))
+}
