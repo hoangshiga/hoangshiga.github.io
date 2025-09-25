77a088dbdb044f5e1672168dbb79d77f2839939f
chuonglntm <chuonglntm@gmail.com>
2025-09-25 21:25:52 +0900

    ./index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-switch (location.pathname) {
-    case '/login/':
+(async () => {
+    if (location.pathname == '/login/') {
+        await new Promise((res, loop) => setTimeout(loop = () => document.body ? res() : setTimeout(loop, 100)))
         var input = document.createElement('input')
         input.type = 'password'
         input.name = 'token'
@@ -7,17 +8,18 @@ switch (location.pathname) {
         button.textContent = 'Login'
         button.onclick = () => (localStorage._token = input.value) && localStorage._redirectUrl && location.replace(localStorage._redirectUrl)
         document.body.append(input, button)
-        break
-    case '/logout/':
+        return
+    }
+    if (location.pathname == '/logout/') {
         delete localStorage._token
         location.replace('/login/')
-        break
-    default:
-        delete localStorage._redirectUrl
-        location.host == 'localhost'
-            ? fetch('/hoangshiga/' + location.pathname.split('/').slice(-2)[0] + '/index.js').then(res => res.text()).then(eval)
-            : fetch('https://api.github.com/repos/hoangshiga/hoangshiga/contents/' + location.pathname.split('/').slice(-2)[0] + '/index.js', {
-                headers: { 'Authorization': 'Bearer ' + localStorage._token }
-            }).then(res => res.json()).then(({ content }) => eval(atob(content)))
-                .catch(ex => (localStorage._redirectUrl = location.href, location.replace('/login/')))
-}
+        return
+    }
+    delete localStorage._redirectUrl
+    location.host == 'localhost'
+        ? fetch('/hoangshiga/' + location.pathname.split('/').slice(-2)[0] + '/index.js').then(res => res.text()).then(eval)
+        : fetch('https://api.github.com/repos/hoangshiga/hoangshiga/contents/' + location.pathname.split('/').slice(-2)[0] + '/index.js', {
+            headers: { 'Authorization': 'Bearer ' + localStorage._token }
+        }).then(res => res.json()).then(({ content }) => eval(atob(content)))
+            .catch(ex => (localStorage._redirectUrl = location.href, location.replace('/login/')))
+})()
