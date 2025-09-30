9a95f601546d04355b2ddb5b1816de1ce1d02cfa
chuonglntm <chuonglntm@gmail.com>
2025-09-30 20:17:52 +0900

    ./index.js

--- index.js
+++ index.js
@@ -32,6 +32,6 @@
         ? fetch('/hoangshiga/' + location.pathname.split('/').slice(-2)[0] + '/index.js').then(res => res.text()).then(eval)
         : fetch('https://api.github.com/repos/hoangshiga/hoangshiga/contents/' + location.pathname.split('/').slice(-2)[0] + '/index.js', {
             headers: { 'Authorization': 'Bearer ' + localStorage._token }
-        }).then(res => res.json()).then(({ content }) => eval(atob(content)))
+        }).then(res => console.log(res) || res.ok ? res.json() : { content: '' }).then(({ content }) => eval(atob(content)))
             .catch(ex => (localStorage._redirectUrl = location.href, location.replace('/login/')))
 })()
