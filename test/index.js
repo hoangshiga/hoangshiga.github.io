8f601c898761b7113c467fb5ab36aaabe8f28e96
chuonglntm <chuonglntm@gmail.com>
2025-10-13 21:02:03 +0900

    test/index.js

--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
-console.log('test')
-document.body.append(Object.assign(document.createElement('button'), {
-    textContent: 'Download',
-    onclick: () => {
-        const url = URL.createObjectURL(new Blob(['test ok']))
-        const a = document.createElement('button')
-        a.href = url
-        a.style = 'display: none'
-        a.download = 'test.txt'
-        document.body.append(a)
-        a.click()
-        a.remove()
-        URL.revokeObjectURL(url)
-    }
-}))
+(async () => {
+    console.log('test')
+    document.body.append(Object.assign(document.createElement('button'), {
+        textContent: 'Download',
+        onclick: () => {
+            const url = URL.createObjectURL(new Blob(['test ok']))
+            const a = document.createElement('button')
+            a.href = url
+            a.style = 'display: none'
+            a.download = 'test.txt'
+            document.body.append(a)
+            a.click()
+            a.remove()
+            URL.revokeObjectURL(url)
+        }
+    }))
+})()
