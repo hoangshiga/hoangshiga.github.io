207aef6dff243a03fa6a4d4722d34eed2509550d
chuonglntm <chuonglntm@gmail.com>
2025-10-13 20:53:40 +0900

    test/index.js

--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
-alert('test ok 1')
+document.body.append(Object.assign(document.createElement('button'), {
+    textContent: 'Download',
+    onclick: () => {
+        const url = URL.createObjectURL(new Blob([['test ok']]))
+        const a = append(document.body, 'a', { href: url, style: 'display: none', download: 'test.txt' })
+        a.click()
+        a.remove()
+        URL.revokeObjectURL(url)
+    }
+}))
