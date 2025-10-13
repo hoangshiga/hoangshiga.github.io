284363cdbd7096b914ab724ce31892f23bb673ae
chuonglntm <chuonglntm@gmail.com>
2025-10-13 22:08:07 +0900

    test/index.js

--- test/index.js
+++ test/index.js
@@ -1,29 +1,19 @@
 (async () => {
-    const click = HTMLAnchorElement.prototype.click
-    HTMLAnchorElement.prototype.click = function () {
-        if (this.href.startsWith('blob')) {
-            console.log([this.href, this.download])
-            fetch(this.href).then(rs => rs.blob()).then(blob => {
-                console.log('blob', window.blob = blob)
-                const reader = new FileReader();
-                reader.onloadend = () => console.log(reader.result.split(',')[1], this.download);
-                reader.readAsDataURL(blob);
-            })
-        }
-        return click.apply(this, arguments)
-    }
-    // await new Promise(res => setTimeout(res, 1000))
-    console.log('test')
     document.body.append(Object.assign(document.createElement('button'), {
-        textContent: 'Download',
+        textContent: 'Blob',
         onclick: () => {
             const blob = new Blob(['test'], { type: 'text/plain' })
             const link = document.createElement('a')
             link.href = URL.createObjectURL(blob)
             link.download = 'file.txt'
+            document.body.append(link)
             link.click()
             link.remove()
             URL.revokeObjectURL(link.href)
         }
+    }))
+    document.body.append(Object.assign(document.createElement('a'), {
+        textContent: 'File',
+        href: 'https://kitsunekko.net/subtitles/japanese/.hack%20G.U/[AME][.hack%20G.U.%20Returner][DVDRIP][BIG5&JIS].jpn.ssa'
     }))
 })()
