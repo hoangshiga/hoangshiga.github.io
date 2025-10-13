b3140cc3380301b931d173b0c1c3c57516e21db2
chuonglntm <chuonglntm@gmail.com>
2025-10-13 21:00:00 +0900

    test/index.js

--- test/index.js
+++ test/index.js
@@ -3,11 +3,11 @@ document.body.append(Object.assign(document.createElement('button'), {
     textContent: 'Download',
     onclick: a => {
         const url = URL.createObjectURL(new Blob(['test ok']))
-        document.body.append(Object.assign(a = document.createElement('button'), {
-            href: url,
-            style: 'display: none',
-            download: 'test.txt'
-        }))
+        const a = document.createElement('button')
+        a.href = url
+        a.style = 'display: none'
+        a.download = 'test.txt'
+        document.body.append(a)
         a.click()
         a.remove()
         URL.revokeObjectURL(url)
