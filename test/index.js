f5bf30767162a50d4248b13238f7a6cdba438832
chuonglntm <chuonglntm@gmail.com>
2025-10-13 20:58:46 +0900

    test/index.js

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ console.log('test')
 document.body.append(Object.assign(document.createElement('button'), {
     textContent: 'Download',
     onclick: a => {
-        const url = URL.createObjectURL(new Blob([['test ok']]))
+        const url = URL.createObjectURL(new Blob(['test ok']))
         document.body.append(Object.assign(a = document.createElement('button'), {
             href: url,
             style: 'display: none',
