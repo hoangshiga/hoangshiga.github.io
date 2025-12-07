ed6b9f8fd37dce742c2b1c9de756ee3445dfc3b5
chuonglntm <chuonglntm@gmail.com>
2025-12-07 21:09:28 +0900

    type/index.js

--- type/index.js
+++ type/index.js
@@ -1,5 +1,5 @@
 document.body.appendChild(Object.assign(document.createElement('textarea'), {
-    style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh'
+    style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; resize: none'
 }))
 document.head.appendChild(Object.assign(document.createElement('script'), {
     src: '../avim.js'
