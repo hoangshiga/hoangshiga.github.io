def0d7cf8ba00154ae1addd09b8faaabfae24f9a
chuonglntm <chuonglntm@gmail.com>
2025-12-29 16:46:08 +0900

    type/index.js

--- type/index.js
+++ type/index.js
@@ -1,6 +1,10 @@
-document.body.appendChild(Object.assign(document.createElement('textarea'), {
+const textarea = document.body.appendChild(Object.assign(document.createElement('textarea'), {
     style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; resize: none; font-family: math; font-size: medium'
 })).focus()
 document.head.appendChild(Object.assign(document.createElement('script'), {
     src: '../avim.js'
 }))
+window.addEventListener('message', async m => {
+    console.log('child', m.data)
+    if (m.data.getValue) window.parent.postMessage({ value: textarea.value }, '*')
+})
