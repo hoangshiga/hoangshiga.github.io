9c1f6a856caf2101bbdb53c56a20a9a4dda3c2c9
chuonglntm <chuonglntm@gmail.com>
2025-12-29 19:26:20 +0900

    type/index.js

--- type/index.js
+++ type/index.js
@@ -1,7 +1,8 @@
 (async () => {
     const append = (p, t, o) => p.append(t = document.createElement(t)) || Object.assign(t, o || {})
     const textarea = append(document.body, 'textarea', {
-        style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; resize: none; font-family: math; font-size: medium'
+        style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; resize: none; font-family: math; font-size: medium',
+        onchange: window.parent.postMessage({ onTypeValueChanged: textarea.value }, '*')
     })
     textarea.focus()
     append(document.head, 'script', { src: '../avim.js' })
