06f8985623e73a75084ff83d36a4e0f4452ccad4
chuonglntm <chuonglntm@gmail.com>
2025-12-29 18:59:00 +0900

    type/index.js

--- type/index.js
+++ type/index.js
@@ -1,11 +1,13 @@
-const textarea = document.body.appendChild(Object.assign(document.createElement('textarea'), {
-    style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; resize: none; font-family: math; font-size: medium'
-})).focus()
-document.head.appendChild(Object.assign(document.createElement('script'), {
-    src: '../avim.js'
-}))
-window.addEventListener('message', async m => {
-    console.log('type', m.data)
-    if (m.data.getTypeValue) window.parent.postMessage({ returnTypeValue: textarea.value }, '*')
-})
-window.parent.postMessage({ typeInit: true }, '*')
+(async () => {
+    const textarea = document.body.appendChild(Object.assign(document.createElement('textarea'), {
+        style: 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; resize: none; font-family: math; font-size: medium'
+    })).focus()
+    document.head.appendChild(Object.assign(document.createElement('script'), {
+        src: '../avim.js'
+    }))
+    window.addEventListener('message', async m => {
+        console.log('type', m.data)
+        if (m.data.getTypeValue) window.parent.postMessage({ returnTypeValue: textarea.value }, '*')
+    })
+    window.parent.postMessage({ typeInit: true }, '*')
+})()
