74b8fb95a69561ea40b6f25d19eb40fa06eb3c9f
chuonglntm <chuonglntm@gmail.com>
2026-01-19 01:20:55 +0900

    recorder/index.js

--- recorder/index.js
+++ recorder/index.js
@@ -13,6 +13,7 @@
         })
     ))
     const modeSelect = document.body.appendChild(document.createElement('select'))
+    console.log(['I+O', 'I', 'O'])
     // ['I+O', 'I', 'O'].forEach(mode => modeSelect.appendChild(
     //     Object.assign(document.createElement('option'), { textContent: mode })
     // ))
