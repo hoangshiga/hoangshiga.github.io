1c86ecbe1affe7c631e19924845e2dbd263bbbc0
chuonglntm <chuonglntm@gmail.com>
2026-01-19 00:26:56 +0900

    recorder/index.js

--- recorder/index.js
+++ recorder/index.js
@@ -1,6 +1,3 @@
-const color = document.createElement('input')
-color.type = 'color';
-color.value = document.body.style.background = localStorage[location.pathname]
-color.oninput = () => [document.body.style.background = localStorage[location.pathname] = color.value]
-document.body.style.textAlign = 'right'
-document.body.append(color)
+(async () => {
+    console.log('test ok')
+})()
