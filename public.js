d356c79066aef3143d53068030c924802793e626
chuonglntm <chuonglntm@gmail.com>
2026-02-03 14:07:50 +0900

    ./public.js

--- public.js
+++ public.js
@@ -1,9 +1,7 @@
-fetch((
-    localStorage._token
-        ? 'https://api.github.com/repos/hoangshiga/hoangshiga.github.io/contents'
-        : 'https://hoangshiga.github.io'
-) + location.pathname + 'index.js', Object.assign({ cache: 'no-cache' },
-    localStorage._token
-        ? { headers: { 'Authorization': 'Bearer ' + localStorage._token } }
-        : {}
-)).then(rs => localStorage._token ? rs.json() : rs.text()).then(rs => eval(localStorage._token ? atob(rs.content) : rs))
+localStorage._token
+    ? fetch('https://api.github.com/repos/hoangshiga/hoangshiga.github.io/contents' + location.pathname + 'index.js',
+        { headers: { 'Authorization': 'Bearer ' + localStorage._token }, cache: 'no-cache' }
+    ).then(rs => rs.json()).then(rs => eval(atob(rs.content)))
+    : fetch('https://hoangshiga.github.io' + location.pathname + 'index.js',
+        { cache: 'no-cache' }
+    ).then(rs => rs.text()).then(rs => eval(rs))
