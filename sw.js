fe56e47e4507a177feebbb538763625a92d6af96
chuonglntm <chuonglntm@gmail.com>
2026-02-13 08:43:41 +0900

    ./sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+(async () => {
+    const append = (p, t, o) => p.append(t = document.createElement(t)) || Object.assign(t, o || {})
+    if (location.pathname == '/' || location.pathname == '/login/') return (async () => {
+        await new Promise((res, loop) => setTimeout(loop = () => document.body ? res() : setTimeout(loop, 100)))
+        const input = append(document.body, 'input', { type: 'password', name: 'token' })
+        append(document.body, 'button', {
+            textContent: '?', onclick: () => (input.type = input.type == 'password' ? 'text' : 'password')
+        })
+        append(document.body, 'button', {
+            textContent: 'Login', onclick: () => {
+                localStorage._token = input.value
+                if (localStorage._redirectUrl) return location.replace(localStorage._redirectUrl)
+                location.reload()
+            }
+        })
+        if (!localStorage._token) return
+        append(document.body, 'a', { textContent: 'Logout', href: '/logout/' })
+    })()
+    if (location.pathname == '/logout/') return (async () => {
+        delete localStorage._token
+        location.replace('/login/')
+        return
+    })()
+    delete localStorage._redirectUrl
+    if (location.host == 'localhost') return (async () => {
+        fetch('/hoangshiga/' + location.pathname.split('/').slice(-2)[0] + '/index.js').then(res => res.text()).then(eval)
+    })()
+    fetch('https://api.github.com/repos/hoangshiga/hoangshiga/contents/' + location.pathname.split('/').slice(-2)[0] + '/index.js', {
+        headers: { 'Authorization': 'Bearer ' + localStorage._token }
+    })
+        .then(res => res.status == 401 ? (localStorage._redirectUrl = location.href, location.replace('/login/')) : res.json())
+        .then(({ content = '' }) => eval(atob(content)))
+        .catch(ex => append(document.body, 'pre', {
+            textContent: ex.stack || ex.message || ex, name: 'token', style: 'font-family: math'
+        }))
+})()
