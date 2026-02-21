6d8c8d8a3c7d17dd7e175319782ef4637b6ac215
chuonglntm <chuonglntm@gmail.com>
2026-02-21 12:36:01 +0900

    open/index.js

--- open/index.js
+++ open/index.js
@@ -1,9 +1,7 @@
 (async () => {
     await new Promise((r, l) => setTimeout(l = () => document.body ? r() : setTimeout(l, 100)))
     document.body.style = 'margin: 0; height: 100%; display: flex; flex-direction: column'
-    const div = document.body.appendChild(Object.assign(document.createElement('div'), {
-        style: 'display: flex; flex-direction: column'
-    }))
+    const div = document.body.appendChild(document.createElement('div'))
     div.appendChild(Object.assign(document.createElement('button'), {
         textContent: 'Open',
         onclick: () => area.value.split("\n").forEach(u => u.trim() && open(u, '_blank'))
