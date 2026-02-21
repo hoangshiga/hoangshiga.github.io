f94e13804c25259afb458fe4c43edd583c1a667b
chuonglntm <chuonglntm@gmail.com>
2026-02-21 12:33:44 +0900

    open/index.js

--- open/index.js
+++ open/index.js
@@ -2,6 +2,7 @@
     await new Promise((r, l) => setTimeout(l = () => document.body ? r() : setTimeout(l, 100)))
     document.body.style = 'margin: 0; height: 100%; display: flex; flex-direction: column'
     document.body.appendChild(Object.assign(document.createElement('button'), {
+        textContent: 'Open',
         onclick: () => area.value.split("\n").forEach(u => u.trim() && open(u, '_blank'))
     }))
     const area = document.body.appendChild(Object.assign(document.createElement('textarea'), {
