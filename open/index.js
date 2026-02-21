befd133d49d7f95ecb064a81e9e439dc5b72d08c
chuonglntm <chuonglntm@gmail.com>
2026-02-21 12:33:23 +0900

    open/index.js

--- open/index.js
+++ open/index.js
@@ -1,10 +1,10 @@
 (async () => {
     await new Promise((r, l) => setTimeout(l = () => document.body ? r() : setTimeout(l, 100)))
-    document.body.style = 'display: flex'
+    document.body.style = 'margin: 0; height: 100%; display: flex; flex-direction: column'
     document.body.appendChild(Object.assign(document.createElement('button'), {
         onclick: () => area.value.split("\n").forEach(u => u.trim() && open(u, '_blank'))
     }))
     const area = document.body.appendChild(Object.assign(document.createElement('textarea'), {
-        style: 'flex: 1'
+        style: 'flex: 1; resize: none'
     }))
 })()
