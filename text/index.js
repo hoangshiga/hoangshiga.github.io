f4782e2c90edfd5f6e7398b93b08277bbe727155
chuonglntm <chuonglntm@gmail.com>
2026-03-12 23:21:33 +0900

    text/index.js

--- text/index.js
+++ text/index.js
@@ -2,22 +2,25 @@
     const append = (p, t, o) => p.append(t = document.createElement(t)) || Object.assign(t, o || {})
     append(document.head, 'meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' })
     const area = append(document.body, 'textarea', { style: 'width: 100%; height: 40%' })
-    const button = append(document.body, 'button', {
+    const okBtn = append(document.body, 'button', {
         textContent: 'OK', onclick: () => {
-            area.remove()
-            button.remove()
+            for (const el of [area, okBtn, loadBtn, clearBtn]) if (el) el.remove()
             append(document.body, 'pre', {
                 style: 'margin: 0; padding: 2px 5px; font-family: math; white-space: break-spaces',
                 textContent: localStorage[location.pathname] = area.value
             })
         }
     })
+    var loadBtn, clearBtn
     if (localStorage[location.pathname]) {
-        append(document.body, 'button', {
+        loadBtn = append(document.body, 'button', {
             textContent: 'Load', onclick: () => [area.value = localStorage[location.pathname]]
         })
-        append(document.body, 'button', {
-            textContent: 'Clear', onclick: () => localStorage.removeItem(location.pathname)
+        clearBtn = append(document.body, 'button', {
+            textContent: 'Clear', onclick: () => {
+                for (const el of [loadBtn, clearBtn]) if (el) el.remove()
+                localStorage.removeItem(location.pathname)
+            }
         })
     }
 })()
