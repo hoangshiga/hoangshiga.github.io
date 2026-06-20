a5d19ce331d66056b2a6271aa9a379aee913f4fc
chuonglntm <chuonglntm@gmail.com>
2026-06-20 22:41:10 +0900

    open/index.js

--- open/index.js
+++ open/index.js
@@ -1,8 +1,10 @@
 (async () => {
-    /*<.wait>*/
+    /*<.wait.append>*/
+/*<.wait>*/
 const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m && !setTimeout(() => lp = rj(new Error(e || 'Timeout')), m) || setTimeout(lp = o => lp && ((o = f()) ? rs(o) : setTimeout(lp, ...is)))) /*</.wait>*/
 /*<.append>*/
     const append = (p, t, o) => (typeof p == 'string' && [[o, t, p] = [t, p]], t = Object.assign(document.createElement(t), o || {}), p && p.append(t), t)/*</.append>*/
+/*</.wait.append>*/
     const body = await wait(() => document.body, 100)
     body.style = 'margin: 0; height: 100%; display: flex; flex-direction: column'
     const div = append(body, 'div')
