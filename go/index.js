730718cca2c360864c1df98a78e149cdb8290f36
chuonglntm <chuonglntm@gmail.com>
2026-06-20 22:50:43 +0900

    go/index.js

--- go/index.js
+++ go/index.js
@@ -1,25 +1,32 @@
 (async () => {
+    /*<.wait.append>*/
+/*<.wait>*/
+const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m && !setTimeout(() => lp = rj(new Error(e || 'Timeout')), m) || setTimeout(lp = o => lp && ((o = f()) ? rs(o) : setTimeout(lp, ...is)))) /*</.wait>*/
+/*<.append>*/
+    const append = (p, t, o) => (typeof p == 'string' && [[o, t, p] = [t, p]], t = Object.assign(document.createElement(t), o || {}), p && p.append(t), t)/*</.append>*/
+/*</.wait.append>*/
     var url = new URLSearchParams(location.search).get('url')
     if (url) {
         url = unescape(url)
-        if (localStorage['/go?url=' + url]) return (location = url)
-        var button = document.createElement('button')
-        button.textContent = 'Activate'
-        button.onclick = () => (localStorage['/go?url=' + url] = true) && (location = url)
-        await new Promise((res, loop) => (loop = () => !document.body && setTimeout(loop) || res())())
-        return document.body.append(button)
+        if (localStorage['/go?url=' + url]) return location = url
+        const body = await wait(() => document.body, 100)
+        return append(body, 'button', {
+            textContent: 'Activate',
+            onclick: () => (localStorage['/go?url=' + url] = true) && (location = url)
+        })
     }
-    var input = document.createElement('input')
-    var button = document.createElement('button')
-    button.textContent = 'Activate'
-    button.onclick = () => input.value && (location = '?url=' + escape(input.value))
-    await new Promise((res, loop) => (loop = () => !document.body && setTimeout(loop) || res())())
-    document.body.append(button, input)
+    const body = await wait(() => document.body, 100)
+    append(body, 'button', {
+        textContent: 'Activate',
+        onclick: () => input.value && (location = '?url=' + escape(input.value))
+    })
+    const input = append(body, 'input')
     Object.entries(localStorage).forEach(([k, v]) => {
         if (!k.startsWith('/go?url=')) return
-        var button = document.createElement('button')
-        button.textContent = 'Remove: ' + k.substr('/go?url='.length)
-        button.onclick = () => localStorage.removeItem(k) || location.reload()
-        document.body.append(document.createElement('br'), button)
+        append(body, 'br')
+        append(body, 'button', {
+            textContent: 'Remove: ' + k.substr('/go?url='.length),
+            onclick: () => localStorage.removeItem(k) || location.reload()
+        })
     })
 })()
