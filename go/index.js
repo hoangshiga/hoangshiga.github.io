6ec3399dddeb8b30a56fc14e0feb2421db26b44d
chuonglntm <chuonglntm@gmail.com>
2026-06-20 23:34:21 +0900

    go/index.js

--- go/index.js
+++ go/index.js
@@ -1,5 +1,10 @@
 (async () => {
-    /*<.wait.append.copy>*/
+    /*<.$.wait.append.copy>*/
+/*<.$>*/
+    const $ = (s, i, ws, x) => (ws = d => !x && d ? (x = d.querySelector(s)) || i && !Array.from(d.querySelectorAll('frame,iframe'), i => ws(i.contentDocument)) || x : x)(document) && x
+    const $$ = (s, i, ws) => (ws = d => d ? [d, ...Array.from(!!i && d.querySelectorAll('frame,iframe'), i => ws(i.contentDocument)).flat()] : [])(document).map(d => [...d.querySelectorAll(s)]).flat()
+    const [log, out, int, en, de] = [Object.assign((...a) => !log.disabled && console.log(...a) || a.pop()), setTimeout, setInterval, encodeURIComponent, decodeURIComponent]
+    const urlStart = (...u) => u.some(u => Array.isArray(u) ? (u[0] || '').startsWith(u[1]) : location.href.startsWith(u)) /*</.$>*/
 /*<.wait>*/
 const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m && !setTimeout(() => lp = rj(new Error(e || 'Timeout')), m) || setTimeout(lp = o => lp && ((o = f()) ? rs(o) : setTimeout(lp, ...is)))) /*</.wait>*/
 /*<.append>*/
@@ -12,8 +17,8 @@ const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m &&
         document.execCommand('copy')
         input.remove()
     } /*</.copy>*/
-/*</.wait.append.copy>*/
-    var url = decodeURIComponent(new URLSearchParams(location.search).get('url') || '')
+/*</.$.wait.append.copy>*/
+    var url = de(new URLSearchParams(location.search).get('url') || '')
     if (url && localStorage['/go?url=' + url]) return location = url
     const body = await wait(() => document.body, 100)
     if (url) {
@@ -25,7 +30,7 @@ const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m &&
     } else {
         append(body, 'button', {
             textContent: 'Activate',
-            onclick: () => input.value && (location = '?url=' + encodeURIComponent(input.value))
+            onclick: () => input.value && (location = '?url=' + en(input.value))
         })
         const input = append(body, 'input')
         Object.entries(localStorage).forEach(([k, v]) => {
@@ -38,7 +43,7 @@ const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m &&
             })
             v = v == 'true' ? url : v
             const update = () => Object.assign(a, {
-                textContent: url + (v == url ? '' : '  |->|  ' + v),
+                textContent: url + (v == url ? '' : '&nbsp;&nbsp;|->|&nbsp;&nbsp;' + v),
                 href: v
             })
             append(body, 'button', {
@@ -53,7 +58,7 @@ const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m &&
             append(body, 'button', {
                 textContent: 'Link',
                 onclick: () => {
-                    const url = location.origin + location.pathname + '?url=' + encodeURIComponent(v)
+                    const url = location.origin + location.pathname + '?url=' + en(v)
                     copy(url)
                     alert('Copied: ' + url)
                 }
