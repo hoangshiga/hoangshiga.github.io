53511dc58e86c6f141b6fb84e91c5782a6763441
chuonglntm <chuonglntm@gmail.com>
2026-08-03 07:15:37 +0900

    open/index.js

--- open/index.js
+++ open/index.js
@@ -1,9 +1,11 @@
 (async () => {
     /*<.wait.append>*/
 /*<.wait>*/
-const wait = (f, i, m, e) => new Promise((rs, rj, lp, is = i ? [i] : []) => m && !setTimeout(() => lp = rj(new Error(e || 'Timeout')), m) || setTimeout(lp = o => lp && ((o = f()) ? rs(o) : setTimeout(lp, ...is)))) /*</.wait>*/
+const wait = (f, i, m, e) => new Promise((rs, rj, lp) => m && !setTimeout(() => lp = rj(new Error(e || 'Timeout')), m) || setTimeout(lp = o => lp && ((o = f()) ? rs(o) : setTimeout(lp, i)))) /*</.wait>*/
 /*<.append>*/
-    const append = (p, t, o) => (typeof p == 'string' && [[o, t, p] = [t, p]], t = Object.assign(document.createElement(t), o || {}), p && p.append(t), t)/*</.append>*/
+        const append = Object.assign((...as) => (Array.isArray(as[0]) ? as : [as]).map(a => append._('append', ...a)).pop(), {
+            _: (f, p, t, o) => (typeof p == 'string' && [[o, t, p] = [t, p]], t = Object.assign(document.createElement(t), o || {}), p && p[f](t), t)
+        })/*</.append>*/
 /*</.wait.append>*/
     const body = await wait(() => document.body, 100)
     body.style = 'margin: 0; height: 100%; display: flex; flex-direction: column'
